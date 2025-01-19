abee064fb0295a2609f5e070222c9566d939c054
Dolapo Salim Olatoye <96884730+DolapoSalim@users.noreply.github.com>
2025-01-19 22:04:41 +0100

    Create installation.cmd

---
+++ installation.cmd
@@ -0,0 +1,4 @@
+The windows CMD prompts for opencv installation
+conda create --name opencv #To create a virtual envionment
+conda activate opencv-env #to activate virtual environment
+conda install -c conda-forge opencv #to install opencv
